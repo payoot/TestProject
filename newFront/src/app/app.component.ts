364c11b25e99a3c4c070d7507be7573ef3cb0312
payoot <p.pholchan.noly@gmail.com>
2022-07-23 17:26:55 +0700

    update serve

--- newFront/src/app/app.component.ts
+++ newFront/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  _url = 'https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=jGoREF5Y5mgX9DSLnBEOAz&redirect_uri=http://localhost:4200/callback&scope=notify&state=test2';
+  // _url = 'https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=jGoREF5Y5mgX9DSLnBEOAz&redirect_uri=http://localhost:4200/callback&scope=notify&state=test2';
+  _url = 'https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=jSjjh1k9G1NRcnBunJCu1n&redirect_uri=http://localhost:4200/callback&scope=notify&state=test4';
   title = 'newFront';
 }
--- newFront/src/app/callback/callback.component.ts
+++ newFront/src/app/callback/callback.component.ts
@@ -18,92 +18,103 @@ export class CallbackComponent implements OnInit {
   // _header: any;
   name = 'Angular 5';
   data: any;
+  
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
-    // private serve: ServicesService
+    // private serve: ServicesService,
   ) {
-    this.route.queryParams.subscribe((params) => {
-      // console.log("hello world");
-
-      // var xhr = new XMLHttpRequest();
-      // var self = this;
-      // xhr.onreadystatechange = function () {
-      //   if (this.readyState == 4 && this.status == 200) {
-      //     console.log(this.responseText);
-      //     self.data = JSON.parse(this.responseText);
-      //   }
-      // };
-
-      // xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-
-      // xhr.send();
-      
-
-      var data = "grant_type=authorization_code&code="+`${params['code']}`+"&redirect_uri=http://localhost:4200/callback&client_id=RrVsMV8tnyA2JVlaJPmKmM&client_secret=qDxQdhauQQGr3isIElpMS9CUW0rDPxPAkHL5GuYTccW";
-      // console.log(data);
-      
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-
-      xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-          console.log(this.responseText);
-        }
-      });
-
-      xhr.open("POST", "https://notify-bot.line.me/oauth/token");
-      xhr.setRequestHeader("cache-control", "no-cache");
-      xhr.setRequestHeader("postman-token", "160ca9f3-295e-067a-4300-775c70b47e8e");
-      xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-      // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-      xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://notify-bot.line.me/oauth/token');
-      xhr.setRequestHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      xhr.setRequestHeader("Access-Control-Allow-Headers", 'X-Requested-With,content-type');
-      xhr.setRequestHeader("Access-Control-Allow-Credentials", 'true');
-      xhr.send(data);
-
-
-
-
-
-
-
-
-
-
-
-
-    });
-  }
-
-  ngOnInit() {
     // this.route.queryParams.subscribe((params) => {
-    //   // console.log(params['code']);
-    //   // this._data = {
-    //   //   grant_type: 'authorization_code',
-    //   //   code: params['code'],
-    //   //   redirect_uri: 'http://localhost:4200/callback',
-    //   //   client_id: 'RrVsMV8tnyA2JVlaJPmKmM',
-    //   //   client_secret: 'qDxQdhauQQGr3isIElpMS9CUW0rDPxPAkHL5GuYTccW' 
-    //   // };
-    //   // this._header= { 
-    //   //   'content-type': 'application/x-www-form-urlencoded',
-    //   //   'postman-token': '27baf12a-1936-95e1-84a3-c1e4819152a0',
-    //   //   'cache-control': 'no-cache' 
+    //   // console.log("hello world");
+
+    //   // var xhr = new XMLHttpRequest();
+    //   // var self = this;
+    //   // xhr.onreadystatechange = function () {
+    //   //   if (this.readyState == 4 && this.status == 200) {
+    //   //     console.log(this.responseText);
+    //   //     self.data = JSON.parse(this.responseText);
+    //   //   }
     //   // };
 
+    //   // xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
 
-    //   // this.http.post(`https://notify-bot.line.me/oauth/token`, {form: this._data} ,{headers: this._header}).subscribe(async (res) => {
-    //   //   console.log(await res);
-    //   // });
+    //   // xhr.send();
+      
 
-    //   // this.serve.getTEst(params['code']).subscribe((res: any) => {
-    //   //   console.log(res);
-    //   // });
+    //   var data = "grant_type=authorization_code&code="+`${params['code']}`+"&redirect_uri=http://localhost:4200/callback&client_id=RrVsMV8tnyA2JVlaJPmKmM&client_secret=qDxQdhauQQGr3isIElpMS9CUW0rDPxPAkHL5GuYTccW";
+    //   // console.log(data);
+      
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.withCredentials = true;
+
+    //   xhr.addEventListener("readystatechange", function () {
+    //     if (this.readyState === 4) {
+    //       console.log(this.responseText);
+    //     }
+    //   });
+
+    //   xhr.open("POST", "https://notify-bot.line.me/oauth/token");
+    //   xhr.setRequestHeader("cache-control", "no-cache");
+    //   xhr.setRequestHeader("postman-token", "160ca9f3-295e-067a-4300-775c70b47e8e");
+    //   xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    //   // xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //   xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://notify-bot.line.me/oauth/token');
+    //   xhr.setRequestHeader("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //   xhr.setRequestHeader("Access-Control-Allow-Headers", 'X-Requested-With,content-type');
+    //   xhr.setRequestHeader("Access-Control-Allow-Credentials", 'true');
+    //   xhr.send(data);
+
+
+
+
+
+
+
+
+
+
+
+
     // });
   }
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.http.get(`http://localhost:3000/callback?code=${params['code']}`).subscribe((ress) => {
+        console.log('res to front',ress);
+        this._saveData(ress);
+      });
+    });
+  }
+
+  _saveData(_value: any) {
+    let body = new HttpParams();
+    body = body.set('groupline_name', _value.value.target);
+    body = body.set('groupline_key_notify', _value.token);
+    this.http
+      .post<any>(`https://liff.at-mybot.me//grouplines`, body, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      }).subscribe((data) => { console.log(data); });
+          // this.http.post<any>(`https://liff.at-mybot.me//grouplines`, body, { headers: header }).subscribe((res) => {
+  }
+  // fetchToken(code: any) {
+  //   let formData = new URLSearchParams();
+  //   formData.append('grant_type', 'authorization_code');
+  //   formData.append('code', code);
+  //   formData.append('redirect_uri', 'http://localhost:3000/callback');
+  //   formData.append('client_id', 'YOUR_CLIENT_ID');
+  //   formData.append('client_secret', 'YOUR_CLIENT_SECRET');
+
+  //   return fetch('https://notify-bot.line.me/oauth/token', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     body: formData,
+  //   });
+  // }
 
 
 }
